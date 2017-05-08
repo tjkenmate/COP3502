e997528604366ad2bce97e880429a237dfb4a462
TjKenMate <tjkenmate@gmail.com>
2017-05-08 16:37:20 -0400

    XD

--- src/study/thorjohansson/module3/Windchill.java
+++ src/study/thorjohansson/module3/Windchill.java
@@ -10,16 +10,23 @@ public class Windchill {
     public static void main (String[] args){
         double result, temperature, velocity;
 
+        //Set up inpit
         Scanner in = new Scanner(System.in);
 
+        //temperature Input
         System.out.print("Input a temperature between -58 - 41: ");
         temperature = in.nextDouble();
+
+        //Check if temperature is invalid
         if(temperature > 41 || temperature < -58)
             System.out.println("The temperature is invalid");
         else {
+            //Velocity Input
             System.out.print("Input a velocity between 0 - 2: ");
             velocity = in.nextDouble();
+            //Check if velocity is valid
             if(velocity <= 2.0 && velocity > 2){
+                //Calculate and print result
                 result = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(velocity, 0.16) + 0.4275 * temperature * Math.pow(velocity, 0.16);
                 System.out.print("The wind chill temperature is: " + result);
             }
